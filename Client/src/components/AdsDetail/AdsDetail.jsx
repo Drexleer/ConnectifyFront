181675b0c2e31d7c2539e0a24766d424be790578
giselecuello <giselecuello@gmail.com>
2023-11-10 18:06:07 -0300

    detail sin comentarios

--- Client/src/components/AdsDetail/AdsDetail.jsx
+++ Client/src/components/AdsDetail/AdsDetail.jsx
@@ -1,100 +1,97 @@
-// /* eslint-disable no-unused-vars */
-// import { useState } from "react";
-// import {
-//   Button,
-//   Card,
-//   CardContent,
-//   CardMedia,
-//   Badge,
-//   Box,
-//   Grid,
-//   List,
-//   ListItem,
-//   Typography,
-// } from "@mui/material";
-// import MercadoPago from "../Payments/MercadoPago";
-// import "./DetailAd.css";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect } from "react";
-// import { useLocation, useParams } from "react-router-dom";
-// import { fetchDetail } from "../../redux/Slices/detailSlice";
-// import Navbar from "../Navbar/Navbar";
-// import { locationUser } from "../../redux/Slices/persistSlice";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Badge,
+  Box,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import MercadoPago from "../Payments/MercadoPago";
+import "./DetailAd.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { fetchDetail } from "../../redux/Slices/detailSlice";
+import Navbar from "../Navbar/Navbar";
+import { locationUser } from "../../redux/Slices/persistSlice";
 
-// import FavoriteIcon from "@mui/icons-material/Favorite";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-// import StarBorderIcon from "@mui/icons-material/StarBorder";
-// import StarIcon from "@mui/icons-material/Star";
-// import { useAuth0 } from "@auth0/auth0-react";
-// import "./DetailAd.css";
-// import { Link } from "react-router-dom";
-// import {
-//   fetchAddFavorites,
-//   fetchRemoveFavorites,
-// } from "../../redux/Slices/favoritesSlice";
-import Comments from "../CommentsClient/CommentsClients";
-// import ButtonBack from "../Utils/ButtonBack/ButtonBack";
-// import Loading from "../Utils/Loading/Loading";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./DetailAd.css";
+import { Link } from "react-router-dom";
+import {
+  fetchAddFavorites,
+  fetchRemoveFavorites,
+} from "../../redux/Slices/favoritesSlice";
+// import Comments from "../CommentsClient/CommentsClients";
+import ButtonBack from "../Utils/ButtonBack/ButtonBack";
+import Loading from "../Utils/Loading/Loading";
 
 
 const DetailAd = () => {
-  // const { user } = useAuth0();
-  // const { id } = useParams();
-  // const dispatch = useDispatch();
-  // const detail = useSelector((state) => state.detail);
-  // const location = useLocation();
+  const { user } = useAuth0();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const detail = useSelector((state) => state.detail);
+  const location = useLocation();
+  const favorites = useSelector((state) => state.favorites.favoriteProfessionals);
+  const users = useSelector((state) => state.usersLogin.user);
+  const userGoogle = useSelector((state) => state.googleLogin.user);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
 
-  // const favorites = useSelector(
-  //   (state) => state.favorites.favoriteProfessionals
-  // );
-  // const users = useSelector((state) => state.usersLogin.user);
-  // const userGoogle = useSelector((state) => state.googleLogin.user);
-  // const [loading, setLoading] = useState(true);
-  // const [userData, setUserData] = useState(null);
+  const newFav = favorites.some(
+    (favorite) =>
+      favorite.professional._id === detail.detail.creator[0]._id
+  );
 
-  // const newFav = favorites.some(
-  //   (favorite) =>
-  //     favorite?.professional?._id === detail?.detail?.creator[0]?._id
-  // );
+  useEffect(() => {
+    dispatch(fetchDetail(id)).then(() => {
+      setLoading(false);
+    });
+  }, [dispatch, id]);
 
-  // useEffect(() => {
-  //   dispatch(fetchDetail(id)).then(() => {
-  //     setLoading(false);
-  //   });
-  // }, [dispatch, id]);
+  useEffect(() => {
+    dispatch(locationUser(location.pathname));
+  }, [dispatch, location]);
 
-  // useEffect(() => {
-  //   dispatch(locationUser(location.pathname));
-  // }, [dispatch, location]);
+  useEffect(() => {
+    setUserData(user);
+  }, [user]);
 
-  // useEffect(() => {
-  //   setUserData(user);
-  // }, [user]);
+  const handleSaveOrRemoveProfile = () => {
+    let formFav;
+    if (users._id) {
+      formFav = {
+        clientId: users._id,
+        professionalId: detail.detail.creator[0]._id,
+      };
+    } else {
+      formFav = {
+        clientId: userGoogle._id,
+        professionalId: detail.detail.creator[0]._id,
+      };
+    }
 
-  // const handleSaveOrRemoveProfile = () => {
-  //   let formFav;
-  //   if (users._id) {
-  //     formFav = {
-  //       clientId: users._id,
-  //       professionalId: detail.detail.creator[0]._id,
-  //     };
-  //   } else {
-  //     formFav = {
-  //       clientId: userGoogle._id,
-  //       professionalId: detail.detail.creator[0]._id,
-  //     };
-  //   }
-
-  //   if (!newFav) {
-  //     dispatch(fetchAddFavorites(formFav));
-  //   } else {
-  //     dispatch(fetchRemoveFavorites(formFav));
-  //   }
-  // };
+    if (!newFav) {
+      dispatch(fetchAddFavorites(formFav));
+    } else {
+      dispatch(fetchRemoveFavorites(formFav));
+    }
+  };
 
   return (
     <div>
-      {/* <Navbar />
+      <Navbar />
       <div className="principal">
         {loading ? (
           <Loading />
@@ -105,8 +102,8 @@ const DetailAd = () => {
                 <Link to={"/home"}>
                   <ButtonBack />
                 </Link>
-              </div> */}
-              {/* {users.types !== "admin" && users.types !== "professional" && (
+              </div>
+              {users.types !== "admin" && users.types !== "professional" && (
                 <Grid item xs={8} align="left">
                   <Box
                     display="flex"
@@ -126,9 +123,9 @@ const DetailAd = () => {
                     </Button>
                   </Box>
                 </Grid>
-              )} */}
+              )}
 
-              {/* <Grid item xs={12} md={10} sx={{ margin: "16px" }}>
+              <Grid item xs={12} md={10} sx={{ margin: "16px" }}>
                 <Typography
                   fontWeight="900"
                   variant="h3"
@@ -198,9 +195,9 @@ const DetailAd = () => {
           </Grid>
         ) : (
           <div>No hay creadores disponibles.</div>
-        )} */}
-        <Comments />
-      {/* </div> */}
+        )}
+        {/* <Comments /> */}
+      </div>
     </div>
   );
 };
