115638dc0b0ea0304e6065aa4ed0d62d26aa9c77
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-11 16:50:09 -0300

    Funcionalidad Paginacion OK

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,30 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import IconButton from '@mui/material/IconButton';
-import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+import IconButton from "@mui/material/IconButton";
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
 
-import { IoMdRefresh } from 'react-icons/io';
-import { MdPersonSearch } from 'react-icons/md';
-import { useState, useEffect } from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import Login from '../../components/Login/Login';
-import { useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { locationUser } from '../../redux/Slices/persistSlice';
-import Professional from '../../components/Card/Professional';
-import { fetchAds } from '../../redux/Slices/adsSlice';
-import styles from './Home.module.css';
-import Pagination from '../../components/Pagination/Pagination';
-import { fetchFilter } from '../../redux/Slices/FiltersCombinedSlice';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import Footer from '../../components/Footer/Footer';
-import Chat from '../../components/Chat/Chat';
-import ButtonTop from '../../components/Utils/ButtonTop/ButtonTop';
-import Loading from '../../components/Utils/Loading/Loading';
-import { useAuth0 } from '@auth0/auth0-react';
-import { fetchUserLoginWithGoogle } from '../../redux/Slices/loginGoogleSlice';
-import Cover from '../../components/Cover/Cover';
+import { IoMdRefresh } from "react-icons/io";
+import { MdPersonSearch } from "react-icons/md";
+import { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Login from "../../components/Login/Login";
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { locationUser } from "../../redux/Slices/persistSlice";
+import Professional from "../../components/Card/Professional";
+import { fetchAds } from "../../redux/Slices/adsSlice";
+import styles from "./Home.module.css";
+import Pagination from "../../components/Pagination/Pagination";
+import { fetchFilter } from "../../redux/Slices/FiltersCombinedSlice";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import Footer from "../../components/Footer/Footer";
+import Chat from "../../components/Chat/Chat";
+import ButtonTop from "../../components/Utils/ButtonTop/ButtonTop";
+import Loading from "../../components/Utils/Loading/Loading";
+import { useAuth0 } from "@auth0/auth0-react";
+import { fetchUserLoginWithGoogle } from "../../redux/Slices/loginGoogleSlice";
+import Cover from "../../components/Cover/Cover";
 
 const Home = () => {
   //* Declaraciones de variables
@@ -34,11 +34,11 @@ const Home = () => {
   //* Estados locales
   const [containerLogin, setContainerLogin] = useState(false);
   const [priceRange, setPriceRange] = useState([1000, 10000]);
-  const [profession, setProfession] = useState('');
-  const [locationProf, setLocationProf] = useState('');
+  const [profession, setProfession] = useState("");
+  const [locationProf, setLocationProf] = useState("");
   const [popUpLogin, setPopUpLogin] = useState(false);
-  const [sortPrice, setSortPrice] = useState('');
-  const [workLocation, setWorkLocation] = useState('');
+  const [sortPrice, setSortPrice] = useState("");
+  const [workLocation, setWorkLocation] = useState("");
   const [chatOpen, setChatOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -47,7 +47,7 @@ const Home = () => {
   const ads = useSelector((state) => state.ads.ads);
   const { isAuthenticated, user } = useAuth0();
   //traer usuario ya después de iniciar sesión
-  const nickname = user?.nickname || ''; // Usando operador opcional para evitar errores si no está definido
+  const nickname = user?.nickname || ""; // Usando operador opcional para evitar errores si no está definido
   //const email = user?.email || ''; Usar cuando se necesite el email
 
   //* Paginado
@@ -67,74 +67,76 @@ const Home = () => {
   };
 
   useEffect(() => {
-
     if (adsFiltered.length < 1) {
-      localStorage.setItem('locationProf', '')
-      localStorage.setItem('profession', '')
-      localStorage.setItem('priceRange', JSON.stringify([1000, 10000]));
-      localStorage.setItem('workLocation', '')
-      localStorage.setItem('sortPrice', '')
+      localStorage.setItem("locationProf", "");
+      localStorage.setItem("profession", "");
+      localStorage.setItem("priceRange", JSON.stringify([1000, 10000]));
+      localStorage.setItem("workLocation", "");
+      localStorage.setItem("sortPrice", "");
     }
-    
-    const savedLocationProf = localStorage.getItem('locationProf');
+
+    const savedLocationProf = localStorage.getItem("locationProf");
     if (savedLocationProf && adsFiltered.length > 0) {
-      setLocationProf(savedLocationProf)
+      setLocationProf(savedLocationProf);
     }
 
-    const savedProfession = localStorage.getItem('profession');
+    const savedProfession = localStorage.getItem("profession");
     if (savedProfession && adsFiltered.length > 0) {
-      setProfession(savedProfession)
+      setProfession(savedProfession);
     }
 
-    const savedPriceRange = JSON.parse(localStorage.getItem('priceRange'));
+    const savedPriceRange = JSON.parse(localStorage.getItem("priceRange"));
     if (savedPriceRange) {
       setPriceRange(savedPriceRange);
     }
 
-    const savedWorkLocation = localStorage.getItem('workLocation');
+    const savedWorkLocation = localStorage.getItem("workLocation");
     if (savedWorkLocation && adsFiltered.length > 0) {
-      setWorkLocation(savedWorkLocation)
+      setWorkLocation(savedWorkLocation);
     }
 
-    const savedSortPrice = localStorage.getItem('sortPrice');
+    const savedSortPrice = localStorage.getItem("sortPrice");
     if (savedSortPrice && adsFiltered.length > 0) {
-      setSortPrice(savedSortPrice)
+      setSortPrice(savedSortPrice);
     }
+  }, []);
 
-  }, [])
+  useEffect(() => {
+    paginate(1)
+  }, [adsFiltered])
 
   //* Filtros Combinados
   const handleLocation = (e) => {
     e.preventDefault();
     setLocationProf(e.target.value);
 
-    localStorage.setItem('locationProf', e.target.value)
+    localStorage.setItem("locationProf", e.target.value);
   };
 
   const handleProfession = (e) => {
     e.preventDefault();
     setProfession(e.target.value);
 
-    localStorage.setItem('profession', e.target.value)
+    localStorage.setItem("profession", e.target.value);
   };
 
   const handlePriceRangeChange = (value) => {
     setPriceRange(value);
 
-    localStorage.setItem('priceRange', JSON.stringify(value));
+    localStorage.setItem("priceRange", JSON.stringify(value));
   };
 
   const handleRemoteWork = (e) => {
     setWorkLocation(e.target.value);
 
-    localStorage.setItem('workLocation', e.target.value)
+    localStorage.setItem("workLocation", e.target.value);
   };
 
   const handlesortPrice = (e) => {
     e.preventDefault();
     setSortPrice(e.target.value);
 
-    localStorage.setItem('sortPrice', e.target.value)
+    localStorage.setItem("sortPrice", e.target.value);
   };
 
   //* Función para aplicar los filtros
@@ -154,20 +156,18 @@ const Home = () => {
   //* Función para limpiar los filtros da error, por ahora comentada
   const clearFilters = (e) => {
     e.preventDefault();
-    setProfession('');
-    setLocationProf('');
-    setSortPrice('');
+    setProfession("");
+    setLocationProf("");
+    setSortPrice("");
     setPriceRange([1000, 10000]);
-    setWorkLocation('');
+    setWorkLocation("");
     dispatch(fetchAds());
 
-    setCurrentPage(1)
-
-    localStorage.setItem('locationProf', '')
-    localStorage.setItem('profession', '')
-    localStorage.setItem('priceRange', JSON.stringify([1000, 10000]));
-    localStorage.setItem('workLocation', '')
-    localStorage.setItem('sortPrice', '')
+    localStorage.setItem("locationProf", "");
+    localStorage.setItem("profession", "");
+    localStorage.setItem("priceRange", JSON.stringify([1000, 10000]));
+    localStorage.setItem("workLocation", "");
+    localStorage.setItem("sortPrice", "");
   };
 
   //* Función para abrir el chat
@@ -213,28 +213,28 @@ const Home = () => {
         {popUpLogin && (
           <div
             style={{
-              position: 'absolute',
-              width: '25rem',
-              height: '10rem',
-              top: '38%',
-              left: '36%',
-              border: '2px solid black',
-              borderRadius: '20px',
-              display: 'flex',
-              justifyContent: 'space-around',
-              alignItems: 'center',
-              backgroundColor: 'rgba(255,255,255,0.9)',
-              zIndex: '1000',
+              position: "absolute",
+              width: "25rem",
+              height: "10rem",
+              top: "38%",
+              left: "36%",
+              border: "2px solid black",
+              borderRadius: "20px",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+              backgroundColor: "rgba(255,255,255,0.9)",
+              zIndex: "1000",
             }}
           >
             <IconButton
               disableElevation
               style={{
-                position: 'absolute',
-                top: '5px',
-                right: '5px',
-                color: '#000000',
-                fontWeight: 'bold',
+                position: "absolute",
+                top: "5px",
+                right: "5px",
+                color: "#000000",
+                fontWeight: "bold",
               }}
               onClick={handlerCloseLoginPopUp}
             >
@@ -242,10 +242,10 @@ const Home = () => {
             </IconButton>
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
               <h3>Email y/o Password incorrectos</h3>
@@ -259,11 +259,15 @@ const Home = () => {
                 <select
                   className={styles.selectCss}
                   id="ProfesionSearch"
+                  value={profession}
                   onChange={handleProfession}
                 >
-                  <option value="DEFAULT" disabled selected>
-                    Elige una profesión
-                  </option>
+                  {profession === "" && (
+                    <option value="DEFAULT">
+                      Elige una profesión
+                    </option>
+                  )}
+
                   {uniqueProfessions.map((profession, id) => (
                     <option key={id} value={profession}>
                       {profession}
@@ -274,9 +278,10 @@ const Home = () => {
                 <select
                   className={`${styles.selectCss} ${styles.selCity}`}
                   id="LocationSearch"
+                  value={locationProf}
                   onChange={handleLocation}
                 >
-                  <option value="DEFAULT" disabled selected>
+                  <option value="DEFAULT">
                     Elige una ciudad
                   </option>
                   {uniqueLocations.map((locations, id) => (
@@ -295,15 +300,15 @@ const Home = () => {
               <span className={styles.minMax}>Max: ${priceRange[1]}</span>
             </div>
             <Slider
-              trackStyle={{ backgroundColor: 'orange', height: 4 }}
-              railStyle={{ backgroundColor: 'white', height: 4 }}
+              trackStyle={{ backgroundColor: "orange", height: 4 }}
+              railStyle={{ backgroundColor: "white", height: 4 }}
               handleStyle={{
-                borderColor: '#1a659a',
+                borderColor: "#1a659a",
                 height: 10,
                 width: 10,
                 marginLeft: 0,
                 marginTop: -3,
-                backgroundColor: '#ffffff',
+                backgroundColor: "#ffffff",
               }}
               range
               min={1000}
@@ -321,9 +326,10 @@ const Home = () => {
             <select
               className={`${styles.selectCss} ${styles.selectOrder}`}
               id="sortPrice"
+              value={sortPrice}
               onChange={handlesortPrice}
             >
-              <option value="DEFAULT" disabled selected>
+              <option value="DEFAULT">
                 Ordenar:
               </option>
               <option value="asc">Ascendente</option>
@@ -343,9 +349,10 @@ const Home = () => {
             <select
               className={`${styles.selectCss} ${styles.selectRemoto}`}
               id="workLocation"
+              value={workLocation}
               onChange={handleRemoteWork}
             >
-              <option value="DEFAULT" disabled selected>
+              <option value="DEFAULT">
                 Tipo
               </option>
               <option value="Remoto">Remoto</option>
@@ -358,9 +365,9 @@ const Home = () => {
               <button className={styles.applyFilter} onClick={applyFilters}>
                 <MdPersonSearch
                   style={{
-                    fontSize: '2em',
-                    marginLeft: '-0.7rem',
-                    marginTop: '-0.4rem',
+                    fontSize: "2em",
+                    marginLeft: "-0.7rem",
+                    marginTop: "-0.4rem",
                   }}
                 />
               </button>
@@ -382,9 +389,9 @@ const Home = () => {
               >
                 <IoMdRefresh
                   style={{
-                    fontSize: '2em',
-                    marginLeft: '-0.85rem',
-                    marginTop: '-0.3rem',
+                    fontSize: "2em",
+                    marginLeft: "-0.85rem",
+                    marginTop: "-0.3rem",
                   }}
                 />
               </button>
@@ -445,12 +452,12 @@ const Home = () => {
               <img
                 src="https://i.pinimg.com/originals/44/30/c9/4430c9ff73da58c23c823f0ea6b6f64c.gif"
                 alt="Obrero"
-                style={{ width: '400px' }}
+                style={{ width: "400px" }}
               />
               <h2
                 style={{
-                  paddingLeft: '1.5em',
-                  paddingBottom: '5em',
+                  paddingLeft: "1.5em",
+                  paddingBottom: "5em",
                 }}
               >
                 No se encontraron Anuncios
@@ -466,9 +473,9 @@ const Home = () => {
             className="open-chat-button"
             onClick={toggleChat}
             style={{
-              position: 'fixed',
-              bottom: '20px',
-              right: '20px',
+              position: "fixed",
+              bottom: "20px",
+              right: "20px",
               zIndex: 9999, // Asegura que el botón del chat aparezca por encima de otros contenidos
             }}
           >
@@ -477,14 +484,14 @@ const Home = () => {
         ) : null}
         {chatOpen && <Chat nickname={nickname} />}
         {currentAds.length !== 0 || adsFiltered.length !== 0 ? (
-        <Pagination
-          currentPage={currentPage}
-          adsPerPage={adsPerPage}
-          totalAds={adsFiltered.length}
-          onPageChange={paginate}
-          currentAds={currentAds}
-        />
-      ) : null}
+          <Pagination
+            currentPage={currentPage}
+            adsPerPage={adsPerPage}
+            totalAds={adsFiltered.length}
+            onPageChange={paginate}
+            currentAds={currentAds}
+          />
+        ) : null}
         <Footer />
       </div>
     </div>
