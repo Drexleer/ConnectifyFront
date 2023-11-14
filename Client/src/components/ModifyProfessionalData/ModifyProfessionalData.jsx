2a31f35fbc9775b6da04eefca466a1ce765c1642
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-14 01:13:41 -0300

    Datos del profesional ya podes elegir provincia y localidad

--- Client/src/components/ModifyProfessionalData/ModifyProfessionalData.jsx
+++ Client/src/components/ModifyProfessionalData/ModifyProfessionalData.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import miApi from "../../../localidades.json";
+import React, { useState, useEffect } from "react";
 import style from "./ModifyProfessionalData.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchModifyDataProf } from "../../redux/Slices/modifyProfSlice";
 import validationModify from "./validationModify";
 import { AiFillCloseCircle } from "react-icons/ai";
 
-const ModifyProfessionalData = ({setPopUpModify}) => {
+const ModifyProfessionalData = ({ setPopUpModify }) => {
   const profById = useSelector((state) => state.modifyProf.detailProf);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const [form, setForm] = useState({
     name: profById.name,
@@ -31,35 +32,84 @@ const ModifyProfessionalData = ({setPopUpModify}) => {
     location: "",
     province: "",
     description: "",
-  })
+  });
 
-  const [popUpDataProf, setPopUpDataProf] = useState(false)
+  const [popUpDataProf, setPopUpDataProf] = useState(false);
 
   const handlerChange = (e) => {
     const propiedad = e.target.id;
     const valor = e.target.value;
 
     setForm({ ...form, [propiedad]: valor });
-    setError(validationModify({ ...form, [propiedad]: valor }))
+    setError(validationModify({ ...form, [propiedad]: valor }));
   };
 
   const handlerSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const errorValue = Object.values(error);
     const existError = errorValue.some((errorMessage) => errorMessage !== "");
 
     if (existError) {
-      setPopUpDataProf(true)
-    }else{
-      await dispatch(fetchModifyDataProf(form,profById._id))
-      setPopUpModify(false)
+      setPopUpDataProf(true);
+    } else {
+      await dispatch(fetchModifyDataProf(form, profById._id));
+      setPopUpModify(false);
     }
-  }
+  };
 
   const handlerClosePopUpDataProf = () => {
-    setPopUpDataProf(false)
+    setPopUpDataProf(false);
+  };
+
+  // function getProvinces(data) {
+  //   const provinces = data.localidades.map((provincia) => {
+  //     return provincia.provincia.nombre;
+  //   });
+  //   return [...new Set(provinces)];
+  // }
+
+  // const provincesList = getProvinces(miApi, form.province);
+
+  // function getLocation(data) {
+  //   const cities = data.localidades.filter((ciudad) => {
+  //     return ciudad.ciudad.nombre === form.province;
+  //   });
+  //   return [...new Set(cities)];
+  // }
+
+  // const locationList = getProvinces(miApi, form.province);
+
+  function getProvinces(data) {
+    const provinces = data.localidades.map((provincia) => {
+      return provincia.provincia.nombre;
+    });
+    return [...new Set(provinces)];
   }
+
+  const provincesList = getProvinces(miApi);
+
+  const selectedProvParticular = form.province;
+  //console.log('Provincia seleccionada:', selectedProvParticular);
+
+  function selectCitiesByProvince(data, selectedProvince) {
+    const cities = data.localidades.filter((ciudad) => {
+      return ciudad.provincia.nombre === selectedProvince;
+    });
+    const sortedCities = [...new Set(cities)].sort((a, b) =>
+      a.nombre.localeCompare(b.nombre)
+    );
+    return sortedCities;
+  }
+
+  const citiesInSelectedProvince = selectCitiesByProvince(
+    miApi,
+    selectedProvParticular
+  );
+
+  console.log(provincesList);
+  // console.log(locationList);
+  console.log(citiesInSelectedProvince);
 
   return (
     <div className={style.containerPopUp}>
@@ -105,20 +155,38 @@ const ModifyProfessionalData = ({setPopUpModify}) => {
             onChange={handlerChange}
             id="profession"
           />
-          <input
-            type="text"
-            placeholder="Localidad"
-            value={form.location}
-            onChange={handlerChange}
-            id="location"
-          />
-          <input
-            type="text"
-            placeholder="Provincia"
+          <select
+            name="province"
+            id="province"
             value={form.province}
             onChange={handlerChange}
-            id="province"
-          />
+            required
+          >
+            <option value="" disabled>
+              {form.province}
+            </option>
+            {provincesList.map((province) => (
+              <option key={province} value={province}>
+                {province}
+              </option>
+            ))}
+          </select>
+          <select
+            name="location"
+            id="location"
+            value={form.location}
+            onChange={handlerChange}
+            required
+          >
+            <option value="" disabled>
+              {form.location}
+            </option>
+            {citiesInSelectedProvince.map((location, index) => (
+              <option key={index} value={location.nombre}>
+                {location.nombre}
+              </option>
+            ))}
+          </select>
           <input
             type="text"
             placeholder="Descripcion"
@@ -128,18 +196,17 @@ const ModifyProfessionalData = ({setPopUpModify}) => {
           />
           <button type="submit">Modificar</button>
         </form>
-        {
-          popUpDataProf &&
+        {popUpDataProf && (
           <div className={style.containerPopUpDataProf}>
-          <div className={style.popUpDataProf}>
-          <AiFillCloseCircle
-            className={style.btnCloseDataProf}
-            onClick={handlerClosePopUpDataProf}
-          />
-          <h3>Faltan completar Datos</h3>
+            <div className={style.popUpDataProf}>
+              <AiFillCloseCircle
+                className={style.btnCloseDataProf}
+                onClick={handlerClosePopUpDataProf}
+              />
+              <h3>Faltan completar Datos</h3>
+            </div>
           </div>
-        </div>
-        }
+        )}
       </div>
     </div>
   );
