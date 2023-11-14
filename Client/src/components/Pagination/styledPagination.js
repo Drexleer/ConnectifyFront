273e2b17d1c52ab00f300803e3b33de0cf431687
Drexleer <drexleer01@gmail.com>
2023-11-14 18:57:23 -0500

    Nuevo

---
+++ Client/src/components/Pagination/styledPagination.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center; /* Centra verticalmente */
+  margin-bottom: 12em;
+  width: 100%;
+  height: 100px;
+  justify-content: center;
+`;
---
+++ Client/src/redux/Slices/detailSlice.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-useless-catch */
+import { createSlice } from '@reduxjs/toolkit';
+import axiosInstance from '../Utils/AxiosInstance';
+const VITE_API_BASE = import.meta.env.VITE_API_BASE || 'localhost';
+console.log(VITE_API_BASE);
+
+export const detailAd = createSlice({
+  name: 'detailId',
+  initialState: {
+    detail: [],
+  },
+  reducers: {
+    setDetail: (state, action) => {
+      state.detail = action.payload;
+    },
+  },
+});
+
+export const { setDetail } = detailAd.actions;
+
+export default detailAd.reducer;
+
+export const fetchDetail = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axiosInstance.get(`${VITE_API_BASE}/ads/${id}`);
+      dispatch(setDetail(data));
+    } catch (error) {
+      throw error;
+    }
+  };
+};
