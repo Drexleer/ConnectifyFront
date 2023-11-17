8500d237078fb7b7f7378c78fdacfa41081e42be
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-16 23:48:53 -0300

    Detalles Diseño Dash Admin OK

--- Client/src/components/register/register.jsx
+++ Client/src/components/register/register.jsx
@@ -38,6 +38,7 @@ const profesiones = [
   "Abogado",
 ];
 
+// 
 const Registration = () => {
   const navigate = useNavigate();
 
--- Client/src/views/DashboardAdmin/DashboardAdmin.jsx
+++ Client/src/views/DashboardAdmin/DashboardAdmin.jsx
@@ -1,27 +1,21 @@
 import AdminDashboard from "../../components/DashboardData/Dashboard";
 import ProfsForAdmin from "../../components/DashboardData/ProfsForAdmin/ProfsForAdmin";
 import Header from "../../components/DashboardData/Header";
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from "../../components/Navbar/Navbar";
+import style from './DashboardAdmin.module.css'
 
 const DashboardAdmin = () => {
   return (
     <div>
-     <Navbar/>
+      <Navbar />
       <div>
         <Header />
       </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div
-          style={{
-            flex: 5,
-            justifySelf: "center",
-
-            marginLeft: "7%",
-          }}
-        >
+      <div className={style.containerDashAdmin}>
+        <div className={style.containerDashAdminProf}>
           <ProfsForAdmin />
         </div>
-        <div style={{ flex: 5, marginRight: "5%" }}>
+        <div>
           <AdminDashboard />
         </div>
       </div>
