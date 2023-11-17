6b50b8e54b056138571e00087f638b823eee7f0b
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-17 00:47:09 -0300

    Detalles Diseño Admin Prof OK

--- Client/src/components/CardProfileProf/CardProfileProf.jsx
+++ Client/src/components/CardProfileProf/CardProfileProf.jsx
@@ -111,11 +111,11 @@ export function CardProfileProf({ setPopUpModify }) {
         </CardContent>
       </div>
       <CardActions style={{ flex: 1, height: "auto" }}>
-        <Link to="/chat">
+        {/* <Link to="/chat">
           <Button variant="contained" size="small" startIcon={<HelpIcon />}>
             Ayuda
           </Button>
-        </Link>
+        </Link> */}
         <Link>
           <Button
             variant="contained"
